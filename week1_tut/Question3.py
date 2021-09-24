243a0d21d0d275b4e048b473995d94ed2a7f5582
Bubashan <bubashanb1@gmail.com>
2021-09-24 11:22:04 +0100

    Question 03

---
+++ week1_tut/Question3.py
@@ -0,0 +1,11 @@
+# - ppsn : String type as there are letters involved, but should only support alphanumeric
+# - passport Number : String type as there are letters involved, but should only support alphanumeric
+# - Temperature in degrees : String, as it has degree value
+# - CAO number : String type as there are letters involved, but should only support alphanumeric
+# - Miles travelled per day : Decimal
+# - Avg collection of exam grades : Decimal
+# - File path : String, as it could contain special characters such as _
+# - persons middle initial : Char
+# - Biography statement : String, as it could contain special characters
+# - Book abstract : String, as it could contain special characters
+# - Mathematical function : String, as it could contain special characters
