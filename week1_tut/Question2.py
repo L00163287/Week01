eed08308718eab2c8175c5b1754a6f22c0fb8494
Bubashan <bubashanb1@gmail.com>
2021-09-24 10:54:04 +0100

    question 02

---
+++ week1_tut/Question2.py
@@ -0,0 +1,19 @@
+#---
+
+top_icon = "^"
+bottom_icon = "v"
+left_icon = "<"
+right_icon = ">"
+
+print(top_icon*30)
+print(left_icon+"\t \t \t \t \t \t \t"+right_icon)
+print(left_icon+"\t \t \t \t \t \t \t"+right_icon)
+print(left_icon+"\t \t \t \t \t \t \t"+right_icon)
+print(left_icon+"\t"+"Title \t \t \t \t \t"+right_icon)
+print(left_icon+"\t"+"A option a\t \t \t \t"+right_icon)
+print(left_icon+"\t"+"B option b\t \t \t \t"+right_icon)
+print(left_icon+"\t"+"C option c\t \t \t \t"+right_icon)
+print(left_icon+"\t \t \t \t \t \t \t"+right_icon)
+print(left_icon+"\t \t \t \t \t \t \t"+right_icon)
+print(left_icon+"\t \t \t \t \t \t \t"+right_icon)
+print(bottom_icon*30)
