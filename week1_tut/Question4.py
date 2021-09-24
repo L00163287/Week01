f423c1aad405f27c24347c4be350bf97e7f87fb7
Bubashan <bubashanb1@gmail.com>
2021-09-24 11:33:04 +0100

    Question 04

---
+++ week1_tut/Question4.py
@@ -0,0 +1,18 @@
+ans_1 = 6 + 8 / 2 - 1 # 14
+print(ans_1) # 9.0
+ans_2 = 1 + 1 - 1 + 2 / 2 # 1.5
+print(ans_2) # 2.0
+ans_3 = 1.1 + 5 / 2 # 3.05
+print(ans_3) # 3.6
+ans_4 = 9.2 / 3
+print(ans_4) # 3.0666666666666664
+ans_5 = 9.2 % 3
+print(ans_5) # 0.1999999999999993
+ans_6 = 1 * 2 + 3 * 5 % 4 # 6.125
+print(ans_6) # 5
+ans_7 = 1 + 8 % 3 * 2 - 9 # -4
+print(ans_7) # -4
+ans_8 = 6 + (24 % (16 -11)) #10
+print(ans_8) #10
+
+#Reason : Python executes from the LH to RH, and calculates every actions, not using bodmas methods
